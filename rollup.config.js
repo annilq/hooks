18237ab7f270169b9d2672dcd99c2e69808c130a
annilq <410493517@qq.com>
2022-06-22 16:50:36 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,9 @@
-import merge from 'deepmerge';
 import { createBasicConfig } from '@open-wc/building-rollup';
 import commonjs from 'rollup-plugin-commonjs';
 import react from 'react';
 import reactDom from 'react-dom';
 
-
 const baseConfig = createBasicConfig();
-
-// export default merge(baseConfig, {
-//   input: './out-tsc/src/index.js',
-//   output: {
-//     fileName:"index.js",
-//     dir: 'dist',
-//   },
-//   plugins: [
-//     commonjs({
-//       include: 'node_modules/**',
-//       namedExports: {
-//         react: Object.keys(react),
-//         'react-dom': Object.keys(reactDom)
-//       }
-//     })
-//   ],
-// });
 
 export default {
   input: './out-tsc/src/index.js',
